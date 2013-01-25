709ab92e67424722c599acb12348873b97250e7b
Paul Walker <paul+gitbox@blacksun.org.uk>
2013-01-25 23:41:27 +0000

    add interactive block, fiblist, multiple statements on main
    fiblist for Fibonacci list, declare module, use printf, add likeHaskell
    interactive function, and use 'do' blocks for multiple actions

--- baby.hs
+++ baby.hs
@@ -1,6 +1,10 @@
 -- Exercises/examples/inspired by "Learn you a Haskell for Great Good" (or
 -- whatever it is)
 
+module Main where
+
+import Text.Printf (printf)
+
 doubleMe x = x * 2
 doubleUs x y = doubleMe x + doubleMe y
 
@@ -21,6 +25,10 @@ fib :: Integer -> Integer
 fib n = if n  < 3 then 1
 		else (fib (n - 2)) + (fib (n - 1))
 
+-- Not the most effecient way by a long short
+fiblist :: Integer -> [Integer]
+fiblist n = [ fib x | x <- [1..n]]
+
 fact :: Integer -> Integer
 fact n = 
 	case n of
@@ -32,12 +40,26 @@ len [] = 0
 len (x:xs) = 1 + len xs
 
 rev :: [a] -> [a]
--- rev [] = []
 rev (x:xs) = concatList (rev xs) ([x])
 rev x = x
 
+likeHaskell :: IO()
+likeHaskell = 
+	do 
+		putStrLn "Do you like Haskell? [yes/no]"
+		answer <- getLine
+		case answer of 
+			"yes" -> putStrLn "yay!"
+			"no" -> putStrLn "aw"
+			_ -> putStrLn "sorry, I don't understand."
+
 main :: IO()
-main = do 
+main = 
+	do 
+		-- interactive gets boring if you do it each time
+		-- likeHaskell
 		putStrLn (rev "Hello world")
-		-- let a = [1, 2, 3]
-		-- putStrLn len(a)
+		let a = [1, 2, 3]
+		-- putStrLn (len a) -- type mismatch, can't do that
+		printf "Factorial 5 is %i\n" (fact 5)
+		printf "%i\n" (len a)
