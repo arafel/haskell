e7a53f801e2b4e56634b288f8ff8a50c135b28e7
Paul Walker <paul@blacksun.org.uk>
2013-02-01 17:41:15 +0000

    continue on the 99 problems

--- problems_1to10.hs
+++ problems_1to10.hs
@@ -5,4 +5,29 @@ myLast [x] = x
 myLast (x:xs) = myLast xs
 
 myButLast :: [Integer] -> Integer
+myButLast (x:xs) = 
+    if ((length xs) == 1) then 
+        x
+    else 
+        myButLast xs
 
+-- More elegant, taken from webpage solutions
+myButLast' x = reverse x !! 1
+
+elementAt :: Integer -> [a] -> a
+elementAt n l = element' 1 n l
+    where
+        element' :: Integer -> Integer -> [a] -> a
+        element' i m (x:xs) = 
+            if (i == m) then
+                x
+            else
+                element' (i + 1) m xs
+
+myLength :: [a] -> Integer
+myLength [] = 0
+myLength (x:xs) = 1 + (myLength xs)
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = (myReverse xs) ++ [x]
