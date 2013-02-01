40007aaef684f2f81ed58e6d7f96dd826794b65f
Paul Walker <paul@blacksun.org.uk>
2013-02-01 12:57:53 +0000

    adding myzip and expanding 'main' function to print stuff

--- unzip.hs
+++ unzip.hs
@@ -7,8 +7,16 @@ myunzip x = myunzip' [] [] x
         myunzip' a b [] = (a, b)
         myunzip' a b (x:xs) = myunzip' (a ++ [fst x]) (b ++ [snd x]) xs
 
+myzip :: [a] -> [b] -> [(a, b)]
+myzip [] [] = []
+myzip (x:xs) (y:ys) = [(x,y)] ++ (myzip xs ys)
+
 main :: IO()
 main = do 
         let thelist = [(1,2),(3,4)]
-        -- let a, b = myunzip thelist
-        print (myunzip thelist)
+        let (a, b) = myunzip thelist
+        putStrLn ("Starting list: " ++ (show thelist))
+        putStrLn ("Split into " ++ (show a) ++ " and " ++ (show b))
+        let c = (myzip a b)
+        putStrLn ("Rejoined into " ++ (show c))
+        putStrLn ("Testing - is rejoined list same as original? " ++ show (thelist == c))
