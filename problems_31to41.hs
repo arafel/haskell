4ba838dfc12beeda87fda0480b8e1be17cda0fe2
Paul Walker <paul@blacksun.org.uk>
2014-05-15 21:38:14 +0100

    continue

--- problems_31to41.hs
+++ problems_31to41.hs
@@ -1,7 +1,14 @@
 module Main where
 
 isPrime :: Integer -> Bool
-isPrime n = isPrime' (n / 2)
+isPrime n = isPrime' (n)
     where
         isPrime' :: Integer -> Bool
-        isPrime'
+        isPrime' x = True
+
+main = 
+    do
+        print ("Is 1 prime? " ++ show (isPrime 1))
+        print ("Is 2 prime? " ++ show (isPrime 2))
+        print ("Is 3 prime? " ++ show (isPrime 3))
+        print ("Is 4 prime? " ++ show (isPrime 4))
